11b201f3b9256e161ea5655c12ea32d29d3bada5
Wasabi <ducvanit9697@gmail.com>
2023-04-23 22:15:09 +0700

    Inital, import script, style

---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Footer file
+ * @package Vantravel
+ */
+?>
+<footer></footer>
+<?php wp_footer() ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @package Vantravel
+ */
+get_header();
+?>
+<p>Test</p>
+<?php
+get_footer();
+
+?>
